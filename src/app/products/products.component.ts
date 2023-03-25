353225f4eff2a9bf2a8cfe76fa41dd82efbdc38e
Shahin Zaman <shahinzaman@MacAir.local>
2023-03-26 00:08:10 +0600

    Displayed the categories

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CategoryService } from '../category.service';
 import { ProductService } from '../product.service';
 
 @Component({
@@ -8,8 +9,10 @@ import { ProductService } from '../product.service';
 })
 export class ProductsComponent {
   products$;
+  categories$;
 
-  constructor(productService: ProductService) {
+  constructor(productService: ProductService, categoryService: CategoryService) {
     this.products$ = productService.getAll();
+    this.categories$ = categoryService.getAll();
   }
 }
